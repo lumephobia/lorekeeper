90599a0acc4b1feca43ad6c1dbbbdb35557252b7
lumephobia <63200980+lumephobia@users.noreply.github.com>
2020-08-25 22:51:57 +0200

    migration class rename

---
+++ database/migrations/2020_08_25_153600_user-link-addon.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserLinkAddon extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add columns
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->string('insta', 40)->nullable($value = true);
+            $table->string('house', 60)->nullable($value = true);
+            $table->string('disc', 8)->nullable($value = true);
+            $table->string('arch', 50)->nullable($value = true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            //
+            $table->dropColumn('insta');
+            $table->dropColumn('house');
+            $table->dropColumn('disc');
+            $table->dropColumn('arch');
+        });
+    }
+}
